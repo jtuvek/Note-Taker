b64f83a5ebd80c35444e79d08479dbb33450bd80
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-21 23:29:16 -0400

    installed uuid. Added routes to server.js

--- server.js
+++ server.js
@@ -9,15 +9,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Your API routes and other back-end code here...
+// API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
-// Serve the notes.html and index.html pages
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+
+    const notes = JSON.parse(data);
+    newNote.id = notes.length + 1; // Assign a unique ID
+    notes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to write note data.' });
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+// HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start the server
