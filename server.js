0ab5445fa387e0a970b7ef7e243c447d57c0abfb
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-22 00:24:12 -0400

    added delete route

--- server.js
+++ server.js
@@ -44,6 +44,28 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+// DELETE Route
+app.delete('/api/notes/:id', (req, res) => {
+  const noteIdToDelete = parseInt(req.params.id);
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+
+    let notes = JSON.parse(data);
+    const updatedNotes = notes.filter(note => note.id !== noteIdToDelete);
+
+    fs.writeFile('db/db.json', JSON.stringify(updatedNotes), 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to update note data.' });
+      }
+      res.json({ message: 'Note deleted successfully.' });
+    });
+  });
+});
+
 // HTML Routes
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
